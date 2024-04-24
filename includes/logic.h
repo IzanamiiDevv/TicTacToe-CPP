731c19b85e81aa8b539ec5248d51c869077f87d1
IzanamiiDevv <rafaeloli596@gmail.com>
2024-04-24 10:12:20 +0800

    add methods

--- includes/logic.h
+++ includes/logic.h
@@ -1,5 +1,9 @@
 #pragma once
 
 namespace Logic {
-    
+    void drawBoard(char *spaces);
+    void playerMove(char *spaces, char player);
+    void computerMove(char *spaces, char computer);
+    bool checkWinner(char *spaces, char player,char computer);
+    bool checkTie(char *spaces);
 }
--- src/logic.cpp
+++ src/logic.cpp
@@ -6,5 +6,19 @@
 #include <ctime>
 
 namespace Logic {
-    
+    void drawBoard(char *spaces){
+
+    };
+    void playerMove(char *spaces, char player){
+
+    };
+    void computerMove(char *spaces, char computer){
+
+    };
+    bool checkWinner(char *spaces, char player,char computer){
+
+    };
+    bool checkTie(char *spaces){
+        
+    };
 }
