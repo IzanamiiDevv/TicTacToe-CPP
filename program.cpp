9626eab19534d7d7a8de7110bdcbd1694ef440be
IzanamiiDevv <rafaeloli596@gmail.com>
2024-04-24 10:26:47 +0800

    add main.cpp

--- program.cpp
+++ program.cpp
@@ -9,7 +9,35 @@ using column = std::string;
 
 int main() {
     column spaces[9] = {"","","","","","","","",""};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
 
-    std::cout << "Test" << std::endl;
+    Logic::drawBoard(spaces);
+
+    while(running){
+        Logic::playerMove(spaces, player);
+        Logic::drawBoard(spaces);
+        if(Logic::checkWinner(spaces, player, computer)){
+            running = false;
+            break;
+        }
+        else if(Logic::checkTie(spaces)){
+            running = false;
+            break;
+        }
+
+        Logic::computerMove(spaces, computer);
+        Logic::drawBoard(spaces);
+        if(Logic::checkWinner(spaces, player, computer)){
+            running = false;
+            break;
+        }
+        else if(Logic::checkTie(spaces)){
+            running = false;
+            break;
+        }
+    }
+    std::cout << "Thanks for playing!\n";
     return 0;
 }
