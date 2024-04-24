16008cf1a9375ef6a07ada1f6788d931b1f4c78a
IzanamiiDevv <rafaeloli596@gmail.com>
2024-04-24 10:14:08 +0800

    adjust includes

--- program.cpp
+++ program.cpp
@@ -1,11 +1,12 @@
-//Local Header
-#include "./includes/logic.h"
-
 //Global Header
 #include <iostream>
 #include <ctime>
 
+//Local Header
+#include "./includes/logic.h"
+
 int main() {
+
     std::cout << "Test" << std::endl;
     return 0;
 }
